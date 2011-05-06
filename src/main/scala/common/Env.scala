7154cd90f6907c2b684ed58a3cc5601d508926eb
Yuvi Masory <ymasory@gmail.com>
2011-05-06 19:13:08 -0400

    start compiler instance fuzzer

---
+++ src/main/scala/common/Env.scala
@@ -0,0 +1,20 @@
+package qualac
+
+import scala.util.Properties
+
+object Env {
+
+  val curDir = (new java.io.File(".")).getCanonicalPath
+
+  lazy val scalaVersion: String = {
+    Properties.releaseVersion match {
+      case Some(v) => v
+      case None    => Properties.developmentVersion match {
+        case Some(v) => v
+        case None => throw QualacException("cannot determine Scala version")
+      }
+    }
+  }
+}
+
+case class QualacException(msg: String) extends RuntimeException(msg)
---
+++ src/main/scala/compile/Scalac.scala
@@ -0,0 +1,44 @@
+package qualac.common
+
+import java.io.{File, StringWriter, PrintWriter}
+
+import scala.tools.nsc.{Global, Settings}
+import scala.tools.nsc.reporters.{Reporter, StoreReporter}
+
+import scala.util.Properties
+
+import org.scalacheck._
+
+import qualac.Env.{curDir, scalaVersion}
+
+object Scalac {
+
+  type ScalacMessage = StoreReporter#Info
+  
+  /** Generate a compiler, UAR */
+  lazy val compiler: Gen[Global] = null
+
+  /** Generate a compiler settings configuration, UAR */
+  lazy val settings: Gen[Settings] = null
+
+
+  // def compile(fileName: String): List[ScalacMessage] = {
+  //   val settings = new Settings 
+  //   settings.outputDirs setSingleOutput (curDir + "/target")
+  //   settings.classpath.tryToSet()
+  //   val reporter = new StoreReporter
+  //   val compiler = new Global(settings, reporter)
+  //   (new compiler.Run).compile(List(testPrefix + fileName))
+  //   reporter.infos.toList
+  // }
+
+  // lazy val classPathList = List(
+  //   "project/boot/scala-" + scalaVersion + "/lib/scala-compiler.jar" +
+  //   ":project/boot/scala-" + scalaVersion + "/lib/scala-library.jar")
+
+  /** Generate a *minimal* classpath list (just scala-library.jar and
+   * scala-compiler.jar), uniformly at random) */
+  lazy val minimalClassPathList: Gen[String] = null
+}
+
+
