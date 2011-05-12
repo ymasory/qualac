2a205e5045c0c3c637d0c782b670030a69b209b9
Yuvi Masory <ymasory@gmail.com>
2011-05-11 20:13:42 -0400

    remove threading framework, add failing spec

--- src/main/scala/lex/IdentifierProperties.scala
+++ src/main/scala/lex/IdentifierProperties.scala
@@ -9,16 +9,11 @@ object IdentifierProperties extends Properties("Identifiers") {
 
   val bmpNonChar = Characters.bmpNonChar
 
-  property("scalac speed test") = forAll { (i: Int) => 
-    true
-    // Scalac.compiles("class X")
-  }
-
-  // property("BMP non-chars cannot be an identifier") =
-  //   forAll(Characters.bmpChar) { c =>
-  //     println("chose: " + c)
-  //     val text = LexUtil.codeToString(c)
-  //     Scalac.compiles("class X")
-  //     true
-  //   }
+  property("any BMP char can be an identifier") =
+    forAll(Characters.bmpChar) { c =>
+      val text = LexUtil.codeToString(c)
+      val (errors, warnings, _) =
+        Scalac.compile(text, new java.io.File("out"))
+      (errors == false && warnings == false)
+    }
 }
