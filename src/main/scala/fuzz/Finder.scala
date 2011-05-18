0bd686c83f397aa4e56a5416a24e6988b1919e30
Yuvi Masory <ymasory@gmail.com>
2011-05-17 21:22:39 -0400

    discover Properties

--- src/main/scala/fuzz/Finder.scala
+++ src/main/scala/fuzz/Finder.scala
@@ -1,24 +1,21 @@
 package qualac.fuzz
 
 import org.clapper.classutil._
-import org.scalacheck.Prop
+import org.scalacheck.Properties
 
 object Finder {
 
-  def discoverTests(): List[Prop] = {
+  def discoverTests(): List[Properties] = {
     val finder = ClassFinder()
     val classes = finder.getClasses
     val classMap = ClassFinder classInfoMap classes
     val allProps =
-      ClassFinder.concreteSubclasses("org.scalacheck.Prop", classMap)
+      ClassFinder.concreteSubclasses("org.scalacheck.Properties", classMap)
     val myProps = allProps.filter(_.name.startsWith("qualac.")).toList
     myProps.map { info =>
       val name = info.name
-      val adjName =
-        if (name endsWith "$") name.substring(0, name.length - 1)
-        else name
-      println("instantiate " + adjName)
-      Class.forName(adjName).newInstance.asInstanceOf[Prop]
+      val clazz = Class.forName(name)
+      clazz.getField("MODULE$").get(null).asInstanceOf[Properties]
     }
   }
 }
