b4497f798312c52fe75a7d1244ed40f7a55dbb2b
Yuvi Masory <ymasory@gmail.com>
2011-05-20 23:13:15 -0400

    add string -> code points

--- src/main/scala/lex/Identifiers.scala
+++ src/main/scala/lex/Identifiers.scala
@@ -1,5 +1,7 @@
 package qualac.lex
 
+import LexImplicits.richify
+
 /**
  * ScalaCheck generators for identifiers.
  *
@@ -16,28 +18,38 @@ object Identifiers {
    */
   lazy val reservedWords = reservedAlphas ++ reservedNonAlphas
 
-  private lazy val reservedAlphas =
-    List("abstract", "case", "catch", "class", "def", "do", "else", "extends",
-         "false", "final", "finally", "for", "forSome", "if", "implicit",
-         "import", "lazy", "match", "new", "null", "object", "override",
-         "package", "private", "protected", "return", "sealed",
-         "super", "this", "throw", "trait", "try", "true", "type", "val",
-         "var", "while", "with", "yield")
+  private lazy val reservedAlphas = {
+    val lst =
+      List("abstract", "case", "catch", "class", "def", "do", "else",
+           "extends", "false", "final", "finally", "for", "forSome", "if",
+           "implicit", "import", "lazy", "match", "new", "null", "object",
+           "override", "package", "private", "protected", "return", "sealed",
+           "super", "this", "throw", "trait", "try", "true", "type", "val",
+           "var", "while", "with", "yield")
+    lst.map(_.codePoints)
+  }
 
-  private lazy val reservedNonAlphas =
-    List("_", ":", "=", "=>", "<-", "<:", "<%", ">:", "#", "@", "⇒", "←")
+  private lazy val reservedNonAlphas = {
+    val lst =
+      List("_", ":", "=", "=>", "<-", "<:", "<%", ">:", "#", "@", "⇒", "←")
+    lst.map(_.codePoints)
+  }
+  
 
   /**
    * @spec Example 1.1.1 Here are examples of identifiers
    */
-  lazy val identifierExamples =
-    List("x", "Object", "maxIndex", "p2p", "empty_?", "+", "`yield`",
-         "αρ τη", "_y", "dot_product_*", "__system", "_MAX_LEN_")
+  lazy val identifierExamples = {
+    val lst =
+      List("x", "Object", "maxIndex", "p2p", "empty_?", "+", "`yield`",
+           "αρ τη", "_y", "dot_product_*", "__system", "_MAX_LEN_")
+    lst.map(_.codePoints)
+  }
 
   /** @spec Example 1.1.2 Backquote-enclosed strings are a solution when one
    * needs to access Java identifiers that are reserved words in Scala.
    * For instance, the statement Thread.yield() is illegal, since yield is a
    * reserved word in Scala. However, here’s a work-around:
    */
-  lazy val legalYieldStatement = "Thread.`yield`()"
+  lazy val legalYieldStatement = "Thread.`yield`()".codePoints
 }
--- src/main/scala/lex/LexImplicits.scala
+++ src/main/scala/lex/LexImplicits.scala
@@ -19,6 +19,22 @@ object LexImplicits {
     }
   }
 
+
+  /** `String` wrapper with utility methods .*/
+  case class RicherString(str: String) {
+
+    /** Convert this string to its code points. MUST BE ASCII. */
+    def codePoints: List[CodePoint] = {
+      str.foreach { c =>
+        assert(c.toInt < 128, str + " contains non-ascii")
+      }
+      (0 until str.length).map{ i => str.codePointAt(i) }.toList
+    }
+  }
+
+  /** Convert a `String` to a `RicherString`. */
+  implicit def richify(str: String): RicherString = RicherString(str)
+
   /**
    * Custom exception indicating a UTF string (ideally like "UTF+008F") is
    * malformed.
