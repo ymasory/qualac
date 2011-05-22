a43b79639eaa637035f87eca79074b4df6c200f4
Yuvi Masory <ymasory@gmail.com>
2011-05-21 22:13:50 -0400

    fix sbt path Closes #25

--- src/main/scala/fuzz/Finder.scala
+++ src/main/scala/fuzz/Finder.scala
@@ -14,59 +14,24 @@ object Finder {
 
   val sbtClassDir =
     new File(Env.curDir, "target/scala_" + Env.scalaVersion + "/classes/")
+  val libDir =
+    new File(Env.curDir, "lib_managed/scala_" + Env.scalaVersion + "/compile/")
+  val path = sbtClassDir :: libDir.listFiles().toList
 
   def discoverPropsMatching(Re: Regex): List[Properties] = {
     val jarFinder = ClassFinder()
-    val sbtFinder = ClassFinder(List(sbtClassDir))
+    val sbtFinder = ClassFinder(path)
     val classes = jarFinder.getClasses ++ sbtFinder.getClasses
     val classMap = ClassFinder classInfoMap classes
-    // val allProps = classMap.keys.flatMap { name: String =>
-    //   name match {
-    //     case Re(_) => {
-    //       val clazz: ClassInfo = classMap(name)
-    //       val supClass = clazz.superClassName
-    //       if (supClass == "org.scalacheck.Properties") Some(clazz)
-    //       else None
-    //     }
-    //     case _ => None
-    //   }
-    // }
-    // val myProps = allProps.filter(_.name.startsWith("qualac.")).toList
-    // myProps.map { info =>
-    //   val name = info.name
-    //   val clazz = Class.forName(name)
-    //   clazz.getField("MODULE$").get(null).asInstanceOf[Properties]
-    // }
-    println(concreteSubclasses("org.scalacheck.Properties", classMap).toList)
-    sys.exit
-    null
+    val allProps =
+      ClassFinder.concreteSubclasses("org.scalacheck.Properties", classMap)
+    val myProps = allProps.filter(_.name.startsWith("qualac.")).toList
+    myProps.map { info =>
+      val name = info.name
+      val clazz = Class.forName(name)
+      clazz.getField("MODULE$").get(null).asInstanceOf[Properties]
+    }
   }
 
   def discoverProps() = discoverPropsMatching(".*".r)
-
-  def concreteSubclasses(ancestor: String, classes: Map[String, ClassInfo]):   
-    Iterator[ClassInfo] = {
-    @tailrec def classMatches(ancestorClassInfo: ClassInfo,
-                              classToCheck: ClassInfo): Boolean = {
-      if (classToCheck.name == ancestorClassInfo.name)
-        true
-      else if ((classToCheck.superClassName == ancestorClassInfo.name) ||
-               (classToCheck implements ancestorClassInfo.name))
-        true
-      else {
-        classes.get(classToCheck.superClassName) match {
-          case None            => false
-          case Some(classInfo) => classMatches(ancestorClassInfo,
-                                               classInfo)
-        }
-      }
-    }
-    classes.get(ancestor) match {
-      case None   => Iterator.empty
-      case Some(ci) =>
-        classes.values.toIterator.
-        filter(_.isConcrete).
-        filter(classMatches(ci, _))
-    }
-  }
 }
